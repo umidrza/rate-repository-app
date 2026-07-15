34046e480c68a81d74fd50c50eedff3d5c9665d3
Umid Rza <umidrza4@gmail.com>
2026-07-15 21:29:26 +0400

    Fetch repositories form api

--- src/components/RepositoryList.jsx
+++ src/components/RepositoryList.jsx
@@ -1,6 +1,7 @@
 import { FlatList, View, StyleSheet } from 'react-native';
+import useRepositories from '../hooks/useRepositories';
+
 import RepositoryItem from './RepositoryItem';
-import repositories from '../data/repositories';
 
 const styles = StyleSheet.create({
   separator: {
@@ -11,9 +12,15 @@ const styles = StyleSheet.create({
 const ItemSeparator = () => <View style={styles.separator} />;
 
 const RepositoryList = () => {
+  const { repositories } = useRepositories();
+
+  const repositoryNodes = repositories
+    ? repositories.edges.map(edge => edge.node)
+    : [];
+
   return (
     <FlatList
-      data={repositories}
+      data={repositoryNodes}
       ItemSeparatorComponent={ItemSeparator}
       renderItem={({ item }) => <RepositoryItem item={item} />}
       keyExtractor={(item) => item.id}
