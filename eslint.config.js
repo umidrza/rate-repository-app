24f402490acd1eb99e8a482145fa777c96807519
Umid Rza <umidrza4@gmail.com>
2026-07-19 21:05:28 +0400

    Add Repository List tests

--- eslint.config.js
+++ eslint.config.js
@@ -1,10 +1,15 @@
 // https://docs.expo.dev/guides/using-eslint/
 const { defineConfig } = require('eslint/config');
 const expoConfig = require("eslint-config-expo/flat");
+const pluginJest = require('eslint-plugin-jest');
 
 module.exports = defineConfig([
   expoConfig,
   {
     ignores: ["dist/*"],
-  }
+  },
+  {
+    files: ['**/*.test.js', '**/*.spec.js'],
+    ...pluginJest.configs['flat/recommended'],
+  },
 ]);
--- src/components/RepositoryItem.jsx
+++ src/components/RepositoryItem.jsx
@@ -64,7 +64,7 @@ const Statistic = ({ label, value }) => (
 
 const RepositoryItem = ({ item }) => {
   return (
-    <View style={styles.container}>
+    <View style={styles.container} testID="repositoryItem">
       <View style={styles.header}>
         <Image
           style={styles.avatar}
